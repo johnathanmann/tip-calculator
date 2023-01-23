560ae65f6e4106fde8ed9d19a328ab4cdf42c7bf
Johnny <johnathan.mnn@gmail.com>
2023-01-22 19:16:09 -0600

    Custom tip feild added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './styles.css';
 
 function App() {
+  function findTip(percentage){
+    console.log(percentage)
+  }
   return (
     <div className="app">
       <div id="calculator">
@@ -17,12 +20,14 @@ function App() {
         </div>
         </section>
         <section id="column-2">
-          <button>5%</button>
-          <button>10%</button>
-          <button>20%</button>
-          <button>25%</button>
-          <button>50%</button>
-          <button>Custom</button>
+          <button onClick={findTip(5)}>5%</button>
+          <button onClick={findTip(10)}>10%</button>
+          <button onClick={findTip(20)}>20%</button>
+          <button onClick={findTip(25)}>25%</button>
+          <button onClick={findTip(50)}>50%</button>
+          <button onClick={findTip()}>Custom</button><br/>
+          <label htmlFor="bill"><h3 id="bill-label" className='inline'>Custom Tip%</h3></label>
+          <input type="text" className='inline' id="customTip" name="bill"/>
         </section>
       </div>
     </div>
